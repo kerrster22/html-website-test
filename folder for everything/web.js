d199fe6d04a6f4f9af4a500aa879e63a0425e4ab
Hayden Kerr <kerrster223@gmail.com>
2023-01-20 09:07:39 +0000

    test

--- folder for everything/web.js
+++ folder for everything/web.js
@@ -1,40 +1,38 @@
+let myName;
+let temperature;
+let temp;
+let color;
+function getName() {
+  myName = prompt("what is your name(a-z)");
 
-
-function getName () {
-    let myName = prompt("what is your name");
-    let message;
-    if (myName) {
-        message = "hello " + myName
-    }else {
-        message=""
-    }
-    console.log("hello " + myName) 
+  if (myName) {
+    console.log("hello " + myName);
+  } else {
+    console.log("hello " + myName);
+  }
 }
-getName(); 
+getName();
 
-
-function ChangeColor () {
-    let color = prompt("what color");
-    document.body.style.backgroundColor  = color;
+function ChangeColor() {
+  color = prompt("what color");
+  document.body.style.backgroundColor = color;
 }
 ChangeColor();
 
+function whatsTheTemp() {
+  temperature = prompt("what is the temperature(0-100)");
+  temp;
 
-function whatsTheTemp(){
-    let temperature = prompt("what is the temperature(0-100)")
-    let temp;
-    
-    if (temperature <= 7) {
-        temp = "wow that is cold " + temperature + "°";
-    } else if(temperature <= 35) {
-        temp = "wow is going to be a warm one today " + temperature + "°";
-    } else if(temperature <=101) {
-        temp = "it is an oven outside! " + temperature + "°";
-    }
- } whatsTheTemp();
-
-
+  if (temperature <= 7) {
+    temp = "wow that is cold " + temperature + "°";
+  } else if (temperature <= 35) {
+    temp = "wow is going to be a warm one today " + temperature + "°";
+  } else if (temperature <= 101) {
+    temp = "it is an oven outside! " + temperature + "°";
+  }
+}
+whatsTheTemp();
 
 document.write(myName + " " + temperature + " °");
+
 confirm("have you answered everything ?");
-console.log(temp);
